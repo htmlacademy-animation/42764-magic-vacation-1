6990b7cac41b98a12ea3ae2781c193db00ee4fc3
lyudmila.kotlyar <lyudmila.kotlyar@ottonova.de>
2020-05-03 13:05:23 +0200

    Добавила анимацию блоков

--- source/js/script.js
+++ source/js/script.js
@@ -9,6 +9,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import rules from './modules/rules';
 
 // init modules
 initialise();
@@ -20,6 +21,7 @@ chat();
 result();
 form();
 social();
+rules();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
