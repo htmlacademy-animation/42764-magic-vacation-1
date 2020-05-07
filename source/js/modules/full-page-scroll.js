d5b2778e94d8fbbe21460a67b4554937f470cc57
Keks <keks@htmlacademy.ru>
2020-05-07 13:29:19 +0300

    :cat:

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,6 +1,4 @@
 import throttle from 'lodash/throttle';
-import {titleAccentTypography, dateAccentTypography} from './intro';
-import {historyTitleAccentTypography} from './history';
 
 export default class FullPageScroll {
   constructor() {
@@ -8,19 +6,17 @@ export default class FullPageScroll {
 
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
-    this.fillScreen = document.querySelector(`.fill-screen`);
 
     this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
-    this.onUrlHashChengedHandler = this.onUrlHashChenged.bind(this);
+    this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
 
   init() {
-    document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT));
+    document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, {trailing: true}));
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
 
-    this.onUrlHashChenged();
-    this.changePageDisplay();
+    this.onUrlHashChanged();
   }
 
   onScroll(evt) {
@@ -31,11 +27,10 @@ export default class FullPageScroll {
     }
   }
 
-  onUrlHashChenged() {
+  onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
-    this.handleAnimations();
   }
 
   changePageDisplay() {
@@ -45,25 +40,12 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
-    const isPrizesScreenActive = this.activeScreen === 2;
-
-    if (isPrizesScreenActive) {
-      this.screenElements.forEach((screen) => {
-        this.fillScreen.classList.add(`active`);
-        setTimeout(() => this.hideScreen(screen), 650);
-      });
-
-      this.fillScreen.classList.add(`active`);
-      setTimeout(() => this.showScreen(this.screenElements[this.activeScreen]), 650);
-    } else {
-      this.screenElements.forEach((screen) => {
-        this.fillScreen.classList.remove(`active`);
-        this.hideScreen(screen);
-      });
-
-      this.fillScreen.classList.remove(`active`);
-      this.showScreen(this.screenElements[this.activeScreen]);
-    }
+    this.screenElements.forEach((screen) => {
+      screen.classList.add(`screen--hidden`);
+      screen.classList.remove(`active`);
+    });
+    this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
+    this.screenElements[this.activeScreen].classList.add(`active`);
   }
 
   changeActiveMenuItem() {
@@ -91,35 +73,6 @@ export default class FullPageScroll {
       this.activeScreen = Math.min(this.screenElements.length - 1, ++this.activeScreen);
     } else {
       this.activeScreen = Math.max(0, --this.activeScreen);
-    }
-  }
-
-  hideScreen(screen) {
-    screen.classList.add(`screen--hidden`);
-    screen.classList.remove(`active`);
-  }
-
-  showScreen(screen) {
-    screen.classList.remove(`screen--hidden`);
-    screen.classList.add(`active`);
-  }
-
-  handleAnimations() {
-    const isIntroPage = this.activeScreen === 0;
-    const isHistoryPage = this.activeScreen === 1;
-
-    if (isIntroPage) {
-      setTimeout(() => titleAccentTypography.runAnimation(), 500);
-      setTimeout(() => dateAccentTypography.runAnimation(), 1300);
-    } else {
-      titleAccentTypography.destroyAnimation();
-      dateAccentTypography.destroyAnimation();
-    }
-
-    if (isHistoryPage) {
-      setTimeout(() => historyTitleAccentTypography.runAnimation(), 100);
-    } else {
-      historyTitleAccentTypography.destroyAnimation();
     }
   }
 }
